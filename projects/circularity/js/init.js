8ecaccb0741da8ee280d38dd235892f22f01da4a
josephpowe <josephpowe2@gmail.com>
2022-02-17 18:38:33 +0000

    bouncing box

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -33,15 +33,8 @@ var init = function (window) {
 
 
         // TODO 3 / 8 : Call the drawCircle() function 
-       /*
-       drawCircle();
-       drawCircle();
-       drawCircle();
-       drawCircle();
-       drawCircle();
-       */
-      
-       for(var i = 0; i <= 100; i++){
+    
+        for(var i = 0; i <= 100; i++){
            drawCircle();
        }
     
@@ -56,23 +49,22 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0])
+        /*  physikz.updatePosition(circles[0]);
             physikz.updatePosition(circles[1]);
             physikz.updatePosition(circles[2]);
             physikz.updatePosition(circles[3]);
             physikz.updatePosition(circles[4]);
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           game.checkCirclePosition(circles[0]);
-           game.checkCirclePosition(circles[1]);
-           game.checkCirclePosition(circles[2]);
-           game.checkCirclePosition(circles[3]);
-           game.checkCirclePosition(circles[4]);
-
+            game.checkCirclePosition(circles[0]);
+            game.checkCirclePosition(circles[1]);
+            game.checkCirclePosition(circles[2]);
+            game.checkCirclePosition(circles[3]);
+            game.checkCirclePosition(circles[4]);
+        */      
             // TODO 9 : Iterate over the array
            for (var k = 0; k <= circle.length; k++) {
-            physikz.updatePosition(circles[k])
+            physikz.updatePosition(circles[k]);
             game.checkCirclePosition(circles[k]);
-
            }
         }
         /* 
